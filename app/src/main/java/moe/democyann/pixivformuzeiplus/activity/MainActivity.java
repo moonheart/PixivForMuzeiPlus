f804d806e7798dc7190e22ada353785122309487
moonheart <fzp1205561571@hotmail.com>
2017-10-02 16:15:03 +0800

    + 申请存储权限

--- app/src/main/java/moe/democyann/pixivformuzeiplus/activity/MainActivity.java
+++ app/src/main/java/moe/democyann/pixivformuzeiplus/activity/MainActivity.java
@@ -1,8 +1,10 @@
 package moe.democyann.pixivformuzeiplus.activity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -19,10 +21,33 @@ public class MainActivity extends AppCompatActivity {
     private Button setting_btn;
     private TextView tv_1;
 
+
+    private static final int REQUEST_EXTERNAL_STORAGE = 1;
+    private static String[] PERMISSIONS_STORAGE = {
+            "android.permission.READ_EXTERNAL_STORAGE",
+            "android.permission.WRITE_EXTERNAL_STORAGE" };
+
+    public static void verifyStoragePermissions(Activity activity) {
+
+        try {
+            //检测是否有写的权限
+            int permission = ActivityCompat.checkSelfPermission(activity,
+                    "android.permission.WRITE_EXTERNAL_STORAGE");
+            if (permission != PackageManager.PERMISSION_GRANTED) {
+                // 没有写的权限，去申请写的权限，会弹出对话框
+                ActivityCompat.requestPermissions(activity, PERMISSIONS_STORAGE,REQUEST_EXTERNAL_STORAGE);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        verifyStoragePermissions(this);
 
         open_btn=(Button)findViewById(R.id.open_btn);
         setting_btn=(Button)findViewById(R.id.setting_btn);
